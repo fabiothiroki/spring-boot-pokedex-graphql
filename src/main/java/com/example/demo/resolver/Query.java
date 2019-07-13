a23a4ca709d78cca398361041e15edfc916c9e0d
Fabio Hiroki <fabiothiroki@gmail.com>
2019-07-13 10:30:12 -0300

    Fix dependencies version

--- src/main/java/com/example/demo/resolver/Query.java
+++ src/main/java/com/example/demo/resolver/Query.java
@@ -6,8 +6,6 @@ import com.example.demo.pokemon.PokemonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class Query implements GraphQLQueryResolver {
 
@@ -15,8 +13,7 @@ public class Query implements GraphQLQueryResolver {
     private PokemonRepository repository;
 
     public Pokemon getPokemon(Long id) {
-        Pokemon pokemon = repository.findById(id).orElse(null);
-        return pokemon;
+        return repository.findById(id).orElse(null);
     }
 
 }
