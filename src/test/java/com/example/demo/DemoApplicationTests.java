7d4590ef75741456c78d235c28195892d871899c
Fabio Hiroki <fabiothiroki@gmail.com>
2019-07-17 06:48:10 -0300

    Setup graphql test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.pokemon.PokemonRepository;
+import com.graphql.spring.boot.test.GraphQLTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@GraphQLTest
 public class DemoApplicationTests {
+
+	@MockBean
+	private PokemonRepository pokemonRepository;
 
 	@Test
 	public void contextLoads() {
